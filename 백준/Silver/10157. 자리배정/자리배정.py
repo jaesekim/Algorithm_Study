e2a44924007fbf614c048ffb98d28631fae6ab42
Jaeseok Kim <107056122+jaesekim@users.noreply.github.com>
2023-02-09 09:02:29 +0900

    [Silver IV] Title: 자리배정, Time: 796 ms, Memory: 37880 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.py
+++ 백준/Silver/10157. 자리배정/자리배정.py
@@ -1,21 +1,24 @@
-C, R = map(int, input().split())
-queue= int(input())
-if C * R < queue:
+c, r = map(int, input().split())
+target = int(input())
+
+dx = [0, 1, 0, -1] #상,우,하,좌
+dy = [1, 0, -1, 0]
+num = 0 #계속 더해갈 숫자
+if c * r < target: #타깃 숫자가 공연장 크기보다 클 때
     print(0)
 else:
-    d_row = [0, 1, 0, -1]  # 행을 뺄셈해 줄 변수
-    d_col = [1, 0, -1, 0]  # 열을 뺄셈해 줄 변수
+    graph = [[0] * (r + 1) for _ in range(c + 1)]
     x = 1
     y = 0
-    num = 0
-    idx = 0
-    route = [[0] * (R + 1) for _ in range(C + 1)]
-    while num != queue:
-        if (1 <= x + d_row[idx] <= C) and (1 <= y + d_col[idx]<= R) and (not route[x + d_row[idx]][y + d_col[idx]]):
-            x += d_row[idx]
-            y += d_col[idx]
-            route[x][y] = 1
+    k = 0
+    while target != num:
+        #if num < c: #k는 dx, dy의 인덱스
+
+        if (0 < x+dx[k] <= c) and (0 < y+dy[k] <= r) and (graph[x+dx[k]][y+dy[k]] == 0):
+            x += dx[k]
+            y += dy[k]
+            graph[x][y] += 1
             num += 1
         else:
-            idx = (idx + 1) % 4
+            k = (k + 1) % 4
     print(x, y)
