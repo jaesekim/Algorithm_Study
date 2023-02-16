a3bba16429f6a7244eb1f8d7e1fa732f5f98e3d2
Jaeseok Kim <107056122+jaesekim@users.noreply.github.com>
2023-02-16 16:17:17 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 162 ms, Memory: 58,520 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -1,35 +1,51 @@
-for no in range(1, 11):
-    length = int(input())
-    cal = input()
-    result = ''  # 후위 연산으로 바꾼 결과값을 넣을 빈 문자열
-    stack = []   # 스택을 위한 리스트
-    for i in range(length):
-        if cal[i].isdigit():
-            result += cal[i]
-        # 1. stack 비어있을 때
-        # 2. 새로 들어올 연산자 우선순위 > 스택 맨 위 연산자 우선순위
-        elif not stack or cal[i] == '*' and stack[-1] == '+':
-            stack.append(cal[i])
-        # 1. 새로 스택에 들어올 연산자의 우선순위 == 스택 맨 위에 있는 연산자의 우선순위
-        # 2. 새로 들어올 연산자의 우선순위 < 스택 맨 위에 있는 연산자의 우선순위
-        # -> stack.pop()을 먼저 한 뒤 새로 들어올 것 append()
+"""
+1. 후위 표기식으로 바꾸기/ 숫자는 문자열에 쌓고 연산자는 스택에
+우선 순위의 연산자가 스택의 상단에 있어야 한다.
+동일 순위를 만났을 때 스택 내의 연산자 pop하고 문자열에 넣고 만난 연산자 스택에 넣기
+끝까지 돌았을 때 스택 안의 연산자 문자열에 넣기
+2. 후위 표기법 계산하기 / 문자열 0번째 인덱스부터 스택에 넣어주면서 연산자 만나면 두 번 팝해서
+계산하기. 이때 두번째 팝한 숫자,연산자,첫번째 팝한 숫자 순서
+"""
+# 후위 표기법을 계산하는 함수를 만들어보자
+def calculate(postfix):
+    stack = []
+    for k in postfix:  # 후위표기법 내의 요소를 도는 반복문(인덱스 0부터)
+        #print(type(k))
+        #print(stack)
+        if k == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif k == '-':
+            stack.append(stack.pop() - stack.pop())
+        elif k == '*':
+            stack.append(stack.pop() * stack.pop())
+        elif k == '/':
+            rv = stack.pop()
+            stack.append(stack.pop() / rv)
         else:
-            result += stack.pop()
-            stack.append(cal[i])
+            stack.append(int(k))
+    return stack.pop()
 
-    # stack 남은 연산자 모두 추가해주기
+# 후위 표기법으로 바꿔주자
+for t in range(1, 11):
+    n = int(input())  # 테케 길이
+    infix = input()  # 중위표기법 받기
+    #print(infix)
+    stack = []
+    postfix = ''  # 후위표기법 저장
+    #print(lst)
+    for i in infix:
+        if i == '*': #or i == '/':
+            while stack and stack[-1] == '*':  #or stack[-1] == '/':
+                postfix += stack.pop()
+            stack += i
+        elif i == '+': # 더하기는 그냥 스택에 무언가 있을 때 하나 팝하기만 하기
+            while stack:
+                postfix += stack.pop()
+            stack += i
+        else:  # n이 숫자일 때
+            postfix += i
     while stack:
-        result += stack.pop()
-    # 후위 표기식으로 된 것들 연산
-    for char in result:  # result 맨 앞 부터 참조하면서 계산
-        if char.isdigit():  # 숫자면 스택에 바로 쌓기
-            stack.append(char)
-        else:  # 연산자라면
-            x1 = int(stack.pop())
-            x2 = int(stack.pop())
-            if char == '*':
-                stack.append(x1 * x2)
-            else:
-                stack.append(x1 + x2)
-    answer = stack[-1]
-    print(f"#{no} {answer}")
+        postfix += stack.pop()
+
+
+    print(f'#{t} {calculate(postfix)}')
