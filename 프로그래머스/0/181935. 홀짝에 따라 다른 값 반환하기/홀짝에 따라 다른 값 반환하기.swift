2d4c507163e7b506591d78551713db3d2b29ee1f
Jaeseok Kim <107056122+jaesekim@users.noreply.github.com>
2024-04-03 21:55:23 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.swift
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.swift
@@ -3,13 +3,13 @@ import Foundation
 func solution(_ n:Int) -> Int {
     var result = 0
     if n % 2 == 1{
-        for num in stride(from: 1, to: n+2, by: 2) {
+        for num in stride(from: 1, through: n, by: 2) {
             result += num
         }
         return result
     } else {
         
-        for num in stride(from: 2, to: n+2, by: 2) {
+        for num in stride(from: 2, through: n, by: 2) {
             result += num * num
         }
         return result
