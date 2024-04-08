fd07306cc90abe7fa3c4440a1810bae74c102605
Jaeseok Kim <107056122+jaesekim@users.noreply.github.com>
2024-04-09 02:18:40 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,24 +1,30 @@
-from collections import deque
+import sys
 
-for tc in range(int(input())):
+input = sys.stdin.readline
+
+
+def dfs(x, y, m, n):
+    stack = [(x, y)]
+    while stack:
+        cur = stack.pop()
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = cur[0] + dx, cur[1] + dy
+            if 0 <= nx < m and 0 <= ny < n and field[nx][ny]:
+                field[nx][ny] = 0
+                stack.append((nx, ny))
+
+
+for _ in range(int(input())):
     M, N, K = map(int, input().split())
     field = [[0] * N for _ in range(M)]
-    visited = [[0] * N for _ in range(M)]
-    queue = deque()
-    answer = 0
     for _ in range(K):
-        a, b = map(int, input().split())
-        field[a][b] = 1
-    for i in range(M):
-        for j in range(N):
-            if field[i][j] and not visited[i][j]:
+        i, j = map(int, input().split())
+        field[i][j] = 1
+    answer = 0
+    for x in range(M):
+        for y in range(N):
+            if field[x][y]:
                 answer += 1
-                queue.append((i, j))
-                while queue:
-                    x, y = queue.popleft()
-                    for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                        nx, ny = x + dx, y + dy
-                        if 0 <= nx < M and 0 <= ny < N and field[nx][ny] and not visited[nx][ny]:
-                            visited[nx][ny] = 1
-                            queue.append((nx, ny))
+                field[x][y] = 0
+                dfs(x, y, M, N)
     print(answer)
