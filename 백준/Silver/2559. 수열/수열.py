0557540feba4fa3e0a9429af4d03697e44048750
Jaeseok Kim <107056122+jaesekim@users.noreply.github.com>
2024-04-09 21:50:41 +0900

    [Silver III] Title: 수열, Time: 84 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,14 +1,8 @@
 N, K = map(int, input().split())
-arr = list(map(int, input().split()))
-local_add = 0
-# 인덱스 0 ~ K - 1 까지 값을 넣어준다
-# 첫 구간의 합을 미리 구해준다
-for i in range(K):
-    local_add += arr[i]
-mx_list = [local_add]
-i = 0
-while i < N - K:
-    local_add = local_add - arr[i] + arr[i+K]
-    mx_list.append(local_add)
-    i += 1
-print(max(mx_list))
+temperature = list(map(int, input().split()))
+window = sum(temperature[:K])
+max_value = window
+for day in range(N - K):
+    window = window - temperature[day] + temperature[day + K]
+    max_value = max(max_value, window)
+print(max_value)
