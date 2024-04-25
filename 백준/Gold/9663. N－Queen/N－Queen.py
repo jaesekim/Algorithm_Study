c86427a664061a7a3698a2e4e8c015dec9f34cbb
Jaeseok Kim <107056122+jaesekim@users.noreply.github.com>
2024-04-25 10:31:48 +0900

    [Gold IV] Title: N-Queen, Time: 25780 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,26 @@
-def solution(depth):
-    global cnt  # 맨 아래까지 도덜하면 가능한 횟수 +1
+def backtracking(depth):
+    global answer
     if depth == N:
-        cnt += 1
-    else:
-        for idx in range(N):  # depth 행의 열방향 탐색
-            col[depth] = idx  # depth 번째 행에 놓여있는 열방향 퀸 표시, 계속 초기화
-            if promising(depth):   # 해당 위치가 유효하다면
-                solution(depth+1)  # 다음 깊이로 넘어가기
+        answer += 1
+        return
+    for idx in range(N):
+        if not check_col[idx] and not diagonal_left[idx + depth] and not diagonal_right[N - 1 + idx - depth]:
+            check_col[idx] = 1
+            diagonal_left[idx + depth] = 1
+            diagonal_right[N - 1 + idx - depth] = 1
 
+            backtracking(depth + 1)
 
-def promising(depth):
-    for i in range(depth):
-        # 같은 열이거나 대각선에 겹치는 부분이 있으면 False
-        # col[depth] 현재 깊이보다 작은 깊이에서 같은 값이 나오면 겹친다는 의미
-        # depth & i -> row / col[depth] & col[i] -> column
-        if col[depth] == col[i] or abs(col[depth] - col[i]) == abs(depth - i):
-            return 0
-    return 1
+            check_col[idx] = 0
+            diagonal_left[idx + depth] = 0
+            diagonal_right[N - 1 + idx - depth] = 0
+
 
 
 N = int(input())
-col = [0] * N
-cnt = 0
-solution(0)
-print(cnt)
+check_col = [0] * N
+diagonal_right = [0] * (2 * N - 1)
+diagonal_left = [0] * (2 * N - 1)
+answer = 0
+backtracking(0)
+print(answer)
